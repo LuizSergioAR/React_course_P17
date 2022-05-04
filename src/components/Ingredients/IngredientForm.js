3bae81622df6b9aba9cd776e806564719a44788f
Luiz Sergio Alves Rodrigues <luiz.sergio456@gmail.com>
2022-05-04 16:06:28 -0300

    ✨ Add http requests using hooks

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-
 import Card from "../UI/Card";
+import LoadingIndicator from "../UI/LoadingIndicator";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-	const [inputState, setInputState] = useState({ title: "", amount: "" });
+	const [enteredTitle, setEnteredTitle] = useState("");
+	const [enteredAmount, setEnteredAmount] = useState("");
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		// ...
+		props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
 	};
 
 	return (
@@ -20,13 +21,9 @@ const IngredientForm = React.memo((props) => {
 						<input
 							type="text"
 							id="title"
-							value={inputState.title}
+							value={enteredTitle}
 							onChange={(event) => {
-								const newTitle = event.target.value;
-								setInputState((prevInputState) => ({
-									title: newTitle,
-									amount: prevInputState.amount,
-								}));
+								setEnteredTitle(event.target.value);
 							}}
 						/>
 					</div>
@@ -35,18 +32,15 @@ const IngredientForm = React.memo((props) => {
 						<input
 							type="number"
 							id="amount"
-							value={inputState.amount}
+							value={enteredAmount}
 							onChange={(event) => {
-								const newAmount = event.target.value;
-								setInputState((prevInputState) => ({
-									amount: newAmount,
-									title: prevInputState.title,
-								}));
+								setEnteredAmount(event.target.value);
 							}}
 						/>
 					</div>
 					<div className="ingredient-form__actions">
 						<button type="submit">Add Ingredient</button>
+						{props.loading && <LoadingIndicator />}
 					</div>
 				</form>
 			</Card>
